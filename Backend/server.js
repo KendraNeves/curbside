ae7aebf38c9987e61ff23acc353009cb406de018
Robert Wongus <rob_wo@yahoo.com>
2020-06-20 13:19:59 -0400

    seeds

--- Backend/server.js
+++ Backend/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/credentials", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Project3", {
   useNewUrlParser: true,
   useFindAndModify: false
 });

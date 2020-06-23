20247b927b766cc1ee7c44a081c889b23abbf2d9
Darren Sarra <adriansarra18@gmail.com>
2020-06-23 14:54:38 -0400

    Current working routes.

--- client/src/App.js
+++ client/src/App.js
@@ -4,21 +4,17 @@ import Homepage from "./pages/Homepage";
 import Listings from "./pages/Search";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
-import Upload from "./pages/Upload";
-import NavBar from "./components/Nav"
+import Upload from "./pages/Upload";  
 
 function App() {
   return(
-    <NavBar/>,
     <BrowserRouter>
-    <main>
       <Route exact path={["/", "/homepage"]} component={Homepage} />
       <Route exact path="/map" component={Listings} />
       <Route exact path="/search" component={Listings} />
       <Route exact path="/signin" component={Signin} />
       <Route exact path="/signup" component={Signup} />
       <Route exact path="/upload" component={Upload} />
-      </main>
     </BrowserRouter>
     
   )

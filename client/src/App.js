65219582a03a6baf0b50443052adc96832fa3ecf
Ricardo Shaffer <ricardo.ramiro.shaffer@gmail.com>
2020-06-24 19:57:30 -0400

    removed line 8 on App.js that contained duplicate {upload} component call

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
 import { BrowserRouter, Route} from "react-router-dom";
-import Homepage from "./pages/Search";
-import Upload from "./pages/Upload";
+import Homepage from "./pages/Homepage";
+import Listings from "./pages/Search";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
-
-import Upload from "./pages/Upload";  
-
+import Upload from "./pages/Upload";
+import NavBar from "./components/Nav"
 
 function App() {
   return(
+    <NavBar/>,
     <BrowserRouter>
+    <main>
       <Route exact path={["/", "/homepage"]} component={Homepage} />
-      <Route exact path="/map" component={Search} />
-      <Route exact path="/search" component={Search} />
+      <Route exact path="/map" component={Listings} />
+      <Route exact path="/search" component={Listings} />
       <Route exact path="/signin" component={Signin} />
       <Route exact path="/signup" component={Signup} />
       <Route exact path="/upload" component={Upload} />
+      </main>
     </BrowserRouter>
-    
+
   )
 };
-
 export default App;

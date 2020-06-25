f82e7d038c96f5abd6083af804b9073fb46766ff
Darren Sarra <adriansarra18@gmail.com>
2020-06-24 21:26:46 -0400

    Added an image into the signup pag.

--- client/src/pages/Signup/index.js
+++ client/src/pages/Signup/index.js
@@ -1,12 +1,26 @@
 import React from "react";
 import "./style.css";
+import signup_img from "../../assets/images/img1.png";
 
 function Signup() {
   return(
     <div className="signup">
-      <header className="sign-header">
-        <a className="navbar-brand" href="/">CURBSIDE</a>
+      <header className="log-header">
+        <a className="log-brand" href="/">CURBSIDE</a>
       </header>
+
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-6">
+            <img src={signup_img} alt="toppled over boxes."></img>
+          </div>
+          <div className="col-6">
+            <div className="row">
+              <h2 className="signup-form-title">CREATE AN ACCOUNT</h2>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }

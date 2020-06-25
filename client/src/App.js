ced393edc44326b9d2b4204565dbb719b902b8a2
Kendra Neves <kendraneves@gmail.com>
2020-06-24 20:53:21 -0400

    Map page with map that takes address input and places target circle over area

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,23 @@
 import React from 'react';
-import { BrowserRouter, Route} from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Search from "./pages/Search";
 import Upload from "./pages/Upload";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
+import Map from "./pages/Map"
 
 function App() {
-  return(
+  return (
     <BrowserRouter>
       <Route exact path={["/", "/homepage"]} component={Homepage} />
-      <Route exact path="/map" component={Search} />
+      <Route exact path="/map" component={Map} />
       <Route exact path="/search" component={Search} />
       <Route exact path="/signin" component={Signin} />
       <Route exact path="/signup" component={Signup} />
       <Route exact path="/upload" component={Upload} />
     </BrowserRouter>
-    
+
   )
 };
 

9eeef561050d9ea0e2906bcfd2cf90b0fd6145ca
Darren Sarra <adriansarra18@gmail.com>
2020-06-24 20:33:36 -0400

    Started a signup page. Added a header.

---
+++ client/src/pages/Signup/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Signup() {
+  return(
+    <div className="signup">
+      <header className="sign-header">
+        <a className="navbar-brand" href="/">CURBSIDE</a>
+      </header>
+    </div>
+  )
+}
+
+export default Signup;

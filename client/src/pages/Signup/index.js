49023dfb74a04d5d14052ac6e7aee7d4585961bd
Darren Sarra <adriansarra18@gmail.com>
2020-06-25 01:26:52 -0400

    Added form in the Signup page.

--- client/src/pages/Signup/index.js
+++ client/src/pages/Signup/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./style.css";
 import signup_img from "../../assets/images/img1.png";
+import {Link} from "react-router-dom";
 
 function Signup() {
   return(
@@ -12,11 +13,23 @@ function Signup() {
       <div className="container-fluid">
         <div className="row">
           <div className="col-6">
-            <img src={signup_img} alt="toppled over boxes."></img>
+            <img src={signup_img} alt="boxes toppled over"></img>
           </div>
           <div className="col-6">
             <div className="row">
               <h2 className="signup-form-title">CREATE AN ACCOUNT</h2>
+            </div>
+            <div className="row">
+              <form>
+                  <div className="form-group">
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email"></input>
+                  </div>
+                  <div className="form-group">
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"></input>
+                  </div>
+                  <button type="submit" className="btn btn-primary">SIGN UP</button>
+                  <Link to="/signin">SIGN IN</Link>
+              </form>
             </div>
           </div>
         </div>

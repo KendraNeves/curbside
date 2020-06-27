ab20000d3595ad443c0a0868447e0d8173a484a8
Robert Wongus <rob_wo@yahoo.com>
2020-06-27 11:09:17 -0400

    removed gitignore

--- client/src/pages/Homepage/index.js
+++ client/src/pages/Homepage/index.js
@@ -1,11 +1,11 @@
 import React from "react";
-import Homepage_Nav from "../../components/Homepage_Nav";
+import HomepageNav from "../../components/Homepage_Nav";
 import "./style.css";
 
 function Homepage() {
   return(
     <div className="homepage">
-      <Homepage_Nav />
+      <HomepageNav />
       <h1 className="homepage-name">CURBSIDE</h1>
       <h2 className="tag-line">THE EASIEST WAY TO LET GO</h2>
     </div>

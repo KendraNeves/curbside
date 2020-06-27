e01c6d4f4b78052e8232968f58123a62042511a2
Darren Sarra <adriansarra18@gmail.com>
2020-06-27 12:57:38 -0400

    Updated the signup display.

--- client/src/pages/Signup/index.js
+++ client/src/pages/Signup/index.js
@@ -4,39 +4,7 @@ import signup_img from "../../assets/images/img1.png";
 import {Link} from "react-router-dom";
 
 // function Signup() {
-//   return(
-//     <div className="log">
-//       <header className="log-header">
-//         <a className="log-brand" href="/">CURBSIDE</a>
-//       </header>
-
-//       <div className="container-fluid">
-//         <div className="row">
-//           <div className="col-6">
-//             <img src={signup_img} alt="boxes toppled over"></img>
-//           </div>
-//           <div className="col-6">
-//             <div className="row">
-//               <h2 className="log-title">CREATE AN ACCOUNT</h2>
-//             </div>
-//             <div className="row">
-//               <form>
-//                   <div className="form-group">
-//                     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email"></input>
-//                   </div>
-//                   <div className="form-group">
-//                     <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"></input>
-//                   </div>
-//                   <button type="submit" className="btn">SIGN UP</button>
-//                   <br></br>
-//                   <Link id="signin-link" to="/signin">SIGN IN</Link>
-//               </form>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   )
+//   
 // }
 class Signup extends Component {
   constructor() {
@@ -66,6 +34,37 @@ class Signup extends Component {
 
     console.log("The form was submitted with the following data");
     console.log(this.state);
+  }
+
+  render() {
+    return(
+      <div className="log">
+        <header className="log-header">
+          <a className="log-brand" href="/">CURBSIDE</a>
+        </header>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-6 img-holder">
+            </div>
+            <div className="col-6">
+              <h2 className="log-title">CREATE AN ACCOUNT</h2>
+              <br></br>
+              <form>
+                <div className="form-group">
+                  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email"></input>
+                </div>
+                <div className="form-group">
+                  <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"></input>
+                </div>
+                <button type="submit" className="btn">SIGN UP</button>
+                <br></br>
+                <Link id="signin-link" to="/signin">SIGN IN</Link>
+                </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 

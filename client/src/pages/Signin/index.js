fa9d57fb325407387e7d27e469c1149c638951dd
Darren Sarra <adriansarra18@gmail.com>
2020-06-27 13:21:04 -0400

    Extends Component from class Signin.

--- client/src/pages/Signin/index.js
+++ client/src/pages/Signin/index.js
@@ -1,25 +1,53 @@
 import React from "react";
-import signin_img from "../../assets/images/img1.png";
 import {Link} from "react-router-dom";
 
-function Signin() {
-  return(
-    <div className="log">
-      <header className="log-header">
-        <a className="log-brand" href="/">CURBSIDE  </a>
-      </header>
+// function Signin() {
+//   
+// }
+class Signin extends Component {
+  constructor() {
+    super();
 
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-6">
-            <img src={signin_img} alt="boxes toppled over"></img>
-          </div>
-          <div className="col-6">
-            <div className="row">
-              <h2 className="log-title">SIGN IN</h2>
+    this.state = {
+      email: "",
+      password: ""
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  
+  handleChange(e) {
+    let target = e.target;
+    let value = target.type === "checkbox" ? target.checked : target.value;
+    let name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    console.log("The form was submitted with the following data");
+    console.log(this.state);
+  }
+
+  render() {
+    return(
+      <div className="log">
+        <header className="log-header">
+          <a className="log-brand" href="/">CURBSIDE  </a>
+        </header>
+      
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-6 img-holder">
             </div>
-            <div className="row">
-              <form>
+            <div className="col-6">
+              <h2 className="log-title">SIGN IN</h2>
+                <form>
                   <div className="form-group">
                     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email"></input>
                   </div>
@@ -29,13 +57,12 @@ function Signin() {
                   <button type="submit" className="btn">LOG IN</button>
                   <br></br>
                   <Link id="signin-link" to="/signup">SIGN UP</Link>
-              </form>
+                </form>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
-
 export default Signin;

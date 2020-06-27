6835b3eefe4ecae3b7c19d80c8e7ac84c2d18f79
Kendra Neves <kendraneves@gmail.com>
2020-06-27 13:49:43 -0400

    Navbar component with routing

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -4,22 +4,34 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { BrowserRouter, Route } from 'react-router-dom';
+import "./style.css"
+
 function NavBar(props) {
-    return (
-<header className="navigation">
-<Navbar collapseOnSelect expand="lg" bg="custom-nav" >
-  <Navbar.Brand href="/">Curbside</Navbar.Brand>
-  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <Navbar.Collapse id="responsive-navbar-nav">
-    <Nav className="mr-auto">
-    <Nav.Link href="/upload">Upload</Nav.Link>
-    <Nav.Link href="#"></Nav.Link>
-    <Nav.Link href="/search">Search</Nav.Link>
-  </Nav>
-  </Navbar.Collapse>
-</Navbar>
-</header>
-    )
+  return (
+    <header className="navigation m-4">
+      <Navbar collapseOnSelect expand="lg" bg="custom-nav" >
+        <Navbar.Brand className="p-2" href="/">Curbside</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <BrowserRouter >
+            <Nav className="mr-auto">
+              {/* vvv RegEx so when at "/" (aka homepage), upload and search don't appear */}
+              <Route path={"/(.+)"}>
+                <Nav.Link href="/upload">Upload</Nav.Link>
+                <Nav.Link className="ml-2" href="/search">Search</Nav.Link>
+              </Route>
+            </Nav>
+            <Nav>
+              <Nav.Link className="mr-2" href="/signin">Sign In</Nav.Link>
+              <Nav.Link className="" href="/signup">Sign Up</Nav.Link>
+            </Nav>
+          </BrowserRouter>
+        </Navbar.Collapse>
+      </Navbar>
+
+    </header >
+  )
 }
- 
+
 export default NavBar;

776243200626cd08f42044e055f35d689584f1a9
vsaleem <vic.saleem@gmail.com>
2020-06-27 03:43:40 -0400

    Initial commit with passport authenication changes.

---
+++ Backend/config/passport.js
@@ -0,0 +1,45 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const JwtStrategy = require('passport-jwt');
+const User = require('../models/user');
+
+// JWT
+const cookieExtractor = req => {
+    let token = null;
+    if(req && req.cookies){
+        token=req.cookies["access_token"]
+    }
+    return token;
+}
+
+// authorization
+passport.use(new JwtStrategy({
+    jwtFromRequest: cookieExtractor,
+    secretOrKey: "Curbside"
+},(payload,done)=>{
+    User.findById({_id: payload.sub},(err,user)=>{
+        if(err)
+            return done(err,false);
+        if(user)
+            return done(null,user);
+        else
+            return done(null,false)
+    });
+}));
+
+
+
+// authenticated local strategy using username and password
+
+passport.use(new LocalStrategy((username,password,done)=> {
+    User.findOne({username},(err,user)=>{
+        // something went wrong with database
+        if(err)
+            return done(err);
+        // If no user exist
+        if(!user)
+            return done(null, false);
+        // checks if password is correct
+        user.comparePassword(password,done);
+    })
+}))
--- Backend/server.js
+++ Backend/server.js
@@ -3,10 +3,12 @@ const mongoose = require('mongoose');
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const cookieParser = require('cookie-parser');
 
-
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
@@ -16,6 +18,9 @@ app.use(routes);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/listings", {
 });
+
+const userRouter = require('./routes/User');
+app.use('/user', userRouter);
 
 app.listen(PORT, function() {
     console.log(`🌎 ==> API server now on port ${PORT}!`);

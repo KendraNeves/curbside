376d4d677c9fba59341add49f6e46fb8e40a7bc4
Marian Akiwumi <mfakiwumi1992@yahoo.com>
2020-06-27 11:09:32 -0400

    reverted back and commented out my files

--- client/src/components/ImageBox/imageUpload.js
+++ client/src/components/ImageBox/imageUpload.js
@@ -1,78 +1,78 @@
-import React, {useState} from 'react';
-import Dropzone from 'react-dropzone';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {fas} from '@fortawesome/free-solid-svg-icons';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import Axios from 'axios';
+// import React, {useState} from 'react';
+// import Dropzone from 'react-dropzone';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import {fas} from '@fortawesome/free-solid-svg-icons';
+// import { library } from '@fortawesome/fontawesome-svg-core';
+// import Axios from 'axios';
 
-library.add(fas);
+// library.add(fas);
 
 
-function ImageUpload(props) {
+// function ImageUpload(props) {
 
-    const [Images, setImages] = useState([])
+//     const [Images, setImages] = useState([])
     
-    const onDrop = (files) => {
+//     const onDrop = (files) => {
 
-        // config to retrieve info from backend
-        let formData = new FormData();
-        const config = {
-            header:{'content-tyoe': 'multipart/form-data'}
-        }
-        formData.append('file', files[0]);
+//         // config to retrieve info from backend
+//         let formData = new FormData();
+//         const config = {
+//             header:{'content-tyoe': 'multipart/form-data'}
+//         }
+//         formData.append('file', files[0]);
 
-        //save image inside node server
-        Axios.post('/api/item/uploadImage', formData,config)
-        .then(response => {
-            //gets info from imageApi in routes/api
-            if(response.data.success) {
-                //to save multiple images
-                setImages([...Images,response.data.image])
-                    props.refreshFunciton([...Images,response.data.image])
+//         //save image inside node server
+//         Axios.post('/api/item/uploadImage', formData,config)
+//         .then(response => {
+//             //gets info from imageApi in routes/api
+//             if(response.data.success) {
+//                 //to save multiple images
+//                 setImages([...Images,response.data.image])
+//                     props.refreshFunciton([...Images,response.data.image])
 
-            } else {
-                alert('Failed to save image')
-            }
-        })
+//             } else {
+//                 alert('Failed to save image')
+//             }
+//         })
         
-    }
+//     }
     
-    return (
+//     return (
 
         
-        <div style={{display:'flex', justifyContent:'space-between'}}>
+//         <div style={{display:'flex', justifyContent:'space-between'}}>
             
             
-            {/* box for file upload */}
-            <Dropzone 
-            onDrop={onDrop}
-            multiple={false}
-            maxSize
-            >
-                {({getRootProps, getInputProps}) => (
-                    <div style={{width:'300px', height:'240px', border: '1px solid lightgray', display:'flex',
-                     alignItems:'center', justifyContent:'center'}}
-                        {...getRootProps()}
-                    >
-                        <input {...getInputProps()} />
-                        <FontAwesomeIcon icon="plus" style={{ fontSize:'3rem'}} />
+//             {/* box for file upload */}
+//             <Dropzone 
+//             onDrop={onDrop}
+//             multiple={false}
+//             maxSize
+//             >
+//                 {({getRootProps, getInputProps}) => (
+//                     <div style={{width:'300px', height:'240px', border: '1px solid lightgray', display:'flex',
+//                      alignItems:'center', justifyContent:'center'}}
+//                         {...getRootProps()}
+//                     >
+//                         <input {...getInputProps()} />
+//                         <FontAwesomeIcon icon="plus" style={{ fontSize:'3rem'}} />
 
-                    </div>
-                )}
-            </Dropzone>
+//                     </div>
+//                 )}
+//             </Dropzone>
 
-            {/* Area for image display */}
-            <div style={{display:'flex',width:'350px', height:'240px', overflowX:'scroll'}}>
+//             {/* Area for image display */}
+//             <div style={{display:'flex',width:'350px', height:'240px', overflowX:'scroll'}}>
 
-                <div onClick>
-                    <img />
-                </div>
+//                 <div onClick>
+//                     <img />
+//                 </div>
 
-            </div>
+//             </div>
 
-        </div>
-    )
-}
+//         </div>
+//     )
+// }
 
-export default ImageUpload;
+// export default ImageUpload;
 
--- client/src/pages/Upload/index.js
+++ client/src/pages/Upload/index.js
@@ -1,242 +1,101 @@
-import React, {useState} from 'react';
-import Homepage_Nav from "../../components/Homepage_Nav";
-import ImageUpload from "../../components/ImageBox/imageUpload";
-import "./style.css";
+import React, { useState, useEffect } from "react";
+import DeleteBtn from "../../components/DeleteBtn";
+import { Col, Row, Container } from "../../components/Grid";
+import { List, ListItem } from "../../components/List";
+import { Input, TextArea, FormBtn } from "../../components/Form";
+import API from "../../utilities/API";
+import Nav from '../../components/Nav';
+import "../../App.css";
+import { GoogleMap } from '@react-google-maps/api';
+function Upload() {
 
-
-// Function for dropdown catergories
-
-const Categories = [
-  {key:1, value: "Furniture"},
-  {key:2, value: "Electronics"},
-  {key:3, value: "Apparral"},
-  {key:4, value: "Toys/Games"},
-  {key:5, value: "Books"}
-]
-
-function UploadProduct() {
-
-// functions for entering input values
-const [NameValue, setNameValue] = useState("")
-const [DescriptionValue, setDescriptionValue] = useState("")
-const [LocationValue, setLocationValue] = useState("")
-const [CategoriesValue, setCatergoriesValue] = useState(1)
-const [Images, setImages] = useState([])
-
-//Onchange event every section of the form
-const onNameChange = (event) => {
-  setNameValue(event.currentTarget.value)
-}
-
-const onDescriptionChange = (event) => {
-  setDescriptionValue(event.currentTarget.value)
-}
-
-const onLocationChange = (event) => {
-  setLocationValue(event.currentTarget.value)
-}
-
-const onCategoriesSelectChange = (event) => {
-  setCatergoriesValue(event.currentTarget.value)
-}
-
-const updateImages = (newImages) => {
-  setImages(newImages)
-}
-  return (
-    <div className="uploadpage">
-      <Homepage_Nav />
-      <div style={{maxWidth:'700px', margin:'2rem auto'}}>
-      <div style={{ textAlign:'center', marginBottom:'2rem'}}>
-        
-        <h2>Upload product</h2>
-      </div>
-
-      <form onSubmit>
-        
-        <ImageUpload  refreshFunction={updateImages} />
-
-        <br/>
-        <br/>
-        <label>Name</label>
-        <input onChange={onNameChange}
-        value={NameValue}
-        />
-
-        <br/>
-        <br/>
-        <label>Description</label>
-        <textarea onChange={onDescriptionChange} 
-        value={DescriptionValue}
-        />
-
-        <br/>
-        <br/>
-
-        {/* Use GoogleMap autocomplete for location */}
-        <label>Curb Location</label>
-        <input onChange={onLocationChange}
-        value={LocationValue}
-        />
-
-        <br/>
-        <br/>
-        <label>Category</label>
-        <select onChange={onCategoriesSelectChange}>
-
-          {Categories.map(item => (
-             <option key={item.key} value={CategoriesValue}>
-               {item.value}
-             </option>
-          ))}
-         
-        </select>
-
-        <br/>
-        <br/>
-        <button 
-        onClick
-        >Submit
-
-        </button>
-
-
-
-      </form>
-    </div>
-    </div>
-    
-  )
-}
-
-export default UploadProduct;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { useState, useEffect } from "react";
-// import DeleteBtn from "../../components/DeleteBtn";
-// import { Col, Row, Container } from "../../components/Grid";
-// import { List, ListItem } from "../../components/List";
-// import { Input, TextArea, FormBtn } from "../../components/Form";
-// import API from "../../utilities/API";
-// import Nav from '../../components/Nav';
-// import "../../App.css";
-// import { GoogleMap } from '@react-google-maps/api';
-//function Upload() {
-
-//   // Initialize books as an empty array
-//     const [listings, setListings] = useState([]);
-//     const [formObject, setFormObject] = useState({
-//         listing_title: "",
-//         listing_description:"",
-//         listing_condition: "", 
-//         listing_location: ""
-//     })
+  // Initialize books as an empty array
+    const [listings, setListings] = useState([]);
+    const [formObject, setFormObject] = useState({
+        listing_title: "",
+        listing_description:"",
+        listing_condition: "", 
+        listing_location: ""
+    })
   
-//     useEffect(() => {
-//       loadListings();
-//     }, []);
+    useEffect(() => {
+      loadListings();
+    }, []);
 
-//     function loadListings() {
-//       API.getListings()
-//       .then(response => setListings(response.data))
-//       .catch(error => console.log(error));
-//     };
+    function loadListings() {
+      API.getListings()
+      .then(response => setListings(response.data))
+      .catch(error => console.log(error));
+    };
 
-//     function deleteListing(id){
-//       API.deleteListing(id)
-//         .then(res => loadListings())
-//         .catch(error => console.log(error));
-//     };
-//     function handleInputChange(event){
-//       const {name, value} = event.target;
-//       setFormObject({...formObject, [name]: value })
-//     };
-//     function handleFormSubmit(event){
-//       event.preventDefault();
-//       if(formObject.listing_title && formObject.listing_location){
-//         API.saveListing({
-//           listing_title: formObject.listing_title,
-//           listing_description:formObject.listing_description,
-//           listing_condition: formObject.listing_condition, 
-//           listing_location: formObject.listing_location
-//         })
-//         .then(() => setFormObject({
-//             listing_title: "",
-//             listing_description:"",
-//             listing_condition: "", 
-//             listing_location: ""
-//         }))
-//         .then(() => loadListings())
-//         .catch(error =>console.log(error));
-//       }
-//     }
-//     return (
-//       <div className="App">
-//       <head>
-//       </head>
-//       <Nav />
-//       <Container fluid>
-//         <Row>
-//           <Col size="md-6">
-//             <form>
-//               <Input onChange={handleInputChange} name="listing_title" placeholder="listing title" value={formObject.listing_title}/>
-//               <Input onChange={handleInputChange} name="listing_description" placeholder="listing description" value={formObject.listing_description}/>
-//               <Input onChange={handleInputChange} name="listing_condition" placeholder="condition (used, new, etc.)" value={formObject.listing_condition}/>
-//               <Input onChange={handleInputChange} name="listing_location" placeholder="city, state" value={formObject.listing_location}/>
-//               <FormBtn onClick={handleFormSubmit}> Submit Listing</FormBtn>
-//             </form>
-//           </Col>
-//           <Col size="md-6 sm-12">
+    function deleteListing(id){
+      API.deleteListing(id)
+        .then(res => loadListings())
+        .catch(error => console.log(error));
+    };
+    function handleInputChange(event){
+      const {name, value} = event.target;
+      setFormObject({...formObject, [name]: value })
+    };
+    function handleFormSubmit(event){
+      event.preventDefault();
+      if(formObject.listing_title && formObject.listing_location){
+        API.saveListing({
+          listing_title: formObject.listing_title,
+          listing_description:formObject.listing_description,
+          listing_condition: formObject.listing_condition, 
+          listing_location: formObject.listing_location
+        })
+        .then(() => setFormObject({
+            listing_title: "",
+            listing_description:"",
+            listing_condition: "", 
+            listing_location: ""
+        }))
+        .then(() => loadListings())
+        .catch(error =>console.log(error));
+      }
+    }
+    return (
+      <div className="App">
+      <head>
+      </head>
+      <Nav />
+      <Container fluid>
+        <Row>
+          <Col size="md-6">
+            <form>
+              <Input onChange={handleInputChange} name="listing_title" placeholder="listing title" value={formObject.listing_title}/>
+              <Input onChange={handleInputChange} name="listing_description" placeholder="listing description" value={formObject.listing_description}/>
+              <Input onChange={handleInputChange} name="listing_condition" placeholder="condition (used, new, etc.)" value={formObject.listing_condition}/>
+              <Input onChange={handleInputChange} name="listing_location" placeholder="city, state" value={formObject.listing_location}/>
+              <FormBtn onClick={handleFormSubmit}> Submit Listing</FormBtn>
+            </form>
+          </Col>
+          <Col size="md-6 sm-12">
             
-//             {listings.length ? (
-//               <List>
-//                 {listings.map(listing => {
-//                   return (
-//                   <ListItem key={listing._id}>
-//                     <a href={"/listings/" + listing._id}>
-//                       <strong>
-//                         {listing.listing_title} in {listing.listing_location}
-//                       </strong>
-//                     </a>
-//                     <DeleteBtn onClick={() => deleteListing(listing._id)}/>
-//                   </ListItem>
-//                 )})}
-//               </List>
-//             ) : (
-//               <h3>No Results to Display</h3>
-//             )}
-//           </Col>
-//         </Row>
-//       </Container>
-//       </div>
-//     );
-//   }
+            {listings.length ? (
+              <List>
+                {listings.map(listing => {
+                  return (
+                  <ListItem key={listing._id}>
+                    <a href={"/listings/" + listing._id}>
+                      <strong>
+                        {listing.listing_title} in {listing.listing_location}
+                      </strong>
+                    </a>
+                    <DeleteBtn onClick={() => deleteListing(listing._id)}/>
+                  </ListItem>
+                )})}
+              </List>
+            ) : (
+              <h3>No Results to Display</h3>
+            )}
+          </Col>
+        </Row>
+      </Container>
+      </div>
+    );
+  }
 
-// export default Upload;
+export default Upload;

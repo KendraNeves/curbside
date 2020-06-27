d9aaba6a9d7eb1742227529d213fd381030b26db
Kendra Neves <kendraneves@gmail.com>
2020-06-27 12:05:48 -0400

    Added Listed-Item/index.js

--- client/src/pages/Listed-Item/index.js
+++ client/src/pages/Listed-Item/index.js
@@ -1,22 +1,12 @@
 import React, { Component } from "react";
 import "../../App.css";
-import ItemDescription from "./components/item-description"
-import { Container, Row, Col } from "../../components/Grid/"
-import NavBar from './../../components/Nav/index';
+import ListedItem from './components/listed-item.js';
+import { listedItemData } from './dummy-json';
 
 export default class ListedItemPage extends Component {
   state = {}
 
   render() {
-    return (
-      <>
-        <NavBar />
-        <Container><ItemDescription /></Container>
-        <Container><ItemDescription /></Container>
-        <Container><ItemDescription /></Container>
-        <Container><ItemDescription /></Container>
-      </>
-
-    );
+    return (<ListedItem><listedItemData>{this.props.children}</listedItemData></ListedItem>);
   }
 }

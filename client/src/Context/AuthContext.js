d540f06a5f6623f13e8365e2e563cdb575a96b5e
vsaleem <vic.saleem@gmail.com>
2020-06-28 14:30:41 -0400

    Fixed bug fixes; createdContext and set up Global State to return to App.js

--- client/src/Context/AuthContext.js
+++ client/src/Context/AuthContext.js
@@ -1,26 +1,31 @@
 import React, {createContext,useState,useEffect} from 'react';
 import AuthService from "../Services/AuthServices";
 
+// Provides and consumes global state
 export const AuthContext = createContext();
 
 export default ({ children })=>{
+    // State hooks
     const[user,setUser] =useState(null);
     const[isAuthenicated,setIsAuthenicated] =useState(false);
     const[isLoaded,setIsLoaded] =useState(false);
-
+    
+    // UseEffect hook
     useEffect(()=>{
         AuthService.isAuthenticated().then(data =>{
             setUser(data.user);
             setIsAuthenicated(data.isAuthenicated);
             setIsLoaded(true);
-        });
-    },[])};
+        })
+    }, [] )
 
-    return (
+    // Returns Global State values to App
+    return(
         <div>
-            {!isLoaded ? <h1></h1>:
+            {!isLoaded ? <h1>Loading</h1> : 
             <AuthContext.Provider value={{user,setUser,isAuthenicated,setIsAuthenicated}}>
-                {children}
+                { children }
             </AuthContext.Provider>}
         </div>
     )
+}

42ae5036ecddbb9eb5cb96d19eace900f18f15a7
vsaleem <vic.saleem@gmail.com>
2020-06-28 13:43:35 -0400

    Added onChange and value requirements to user email and password.

--- client/src/pages/Signup/index.js
+++ client/src/pages/Signup/index.js
@@ -1,9 +1,48 @@
-import React from "react";
+import React, {useState, useRef, useEffect} from "react";
+import AuthService from '../../Services/AuthServices';
+import Message from '../../components/Message/index';
 import "./style.css";
 import signup_img from "../../assets/images/img1.png";
 import {Link} from "react-router-dom";
 
-function Signup() {
+function Signup(props) {
+  const [user,setUser] =useState({email: "", password: "", role: ""});
+  const [message,setMessage] = useState(null);
+  let timerID = useRef(null);
+
+  useEffect(()=>{
+    return ()=>{
+      clearTimeout(timerID)
+    }
+  },[]);
+
+  const onChange = e =>{
+    setUser({...user,[e.target.name] : e.target.value});
+    console.log(user)
+  }
+
+  // Reset form
+  const resetForm = ()=>{
+    setUser({email: '', password: '', role: ''})
+  }
+
+  // Displays message to user for 2 seconds; then redirects user to signin page
+  const onSubmit = e =>{
+    e.preventDefault();
+    console.log(e)
+    AuthService.signin(user).then(data=>{
+        const { message } = data;
+        setMessage(message);
+        resetForm();
+        if(!message.msgError){
+          timerID = setTimeout(()=>{
+            props.history.push('/login');
+          }, 2000)
+        }
+    });
+  }
+  
+
   return(
     <div className="log">
       <header className="log-header">
@@ -22,15 +61,27 @@ function Signup() {
             <div className="row">
               <form>
                   <div className="form-group">
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email"></input>
+                    <input type="email" name="email" 
+                            value={user.email}
+                            onChange={onChange}
+                            className="form-control" 
+                            id="exampleInputEmail1" 
+                            aria-describedby="emailHelp" 
+                            placeholder="Email"></input>
                   </div>
                   <div className="form-group">
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"></input>
+                    <input type="password" name="password" 
+                            value={user.password}
+                            onChange={onChange}
+                            className="form-control" 
+                            id="exampleInputPassword1" 
+                            placeholder="Password"></input>
                   </div>
                   <button type="submit" className="btn">SIGN UP</button>
                   <br></br>
                   <Link id="signin-link" to="/signin">SIGN IN</Link>
               </form>
+              {message ? <Message message={message}/> : null}
             </div>
           </div>
         </div>

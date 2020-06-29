84660b9daffbd6590dee660054ee3d3228114a19
vsaleem <vic.saleem@gmail.com>
2020-06-29 19:11:40 -0400

    Bug fixes; corrected /authenticated routes

--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -3,91 +3,102 @@ const userRouter = express.Router();
 const passport = require('passport');
 const passportConfig = require('../config/passport');
 const JWT = require('jsonwebtoken');
-const User = require('../models/user');
 const Listing = require('../models/listing');
 
 
-const signToken = userID =>{
+const signToken = userID => {
     return JWT.sign({
-        iss : "Curbside",
-        sub : userID
-    }, "Curbside", {expiresIn : "12h"});
+        iss: "Curbside",
+        sub: userID
+    }, "Curbside", { expiresIn: "12h" });
 }
 
-userRouter.post('/signup', (req,res)=>{
-    const{username,password,role} = req.body;
-    User.findOne({username},(err,user)=>{
-        if(err)
-            res.status(500).json({message : {msgBody : "Error has occurred.", msgError : true }});
-        if(user)
-            res.status(400).json({ message : {msgBody : "Username is already taken.", msgError : true }});
-        else{
-            const newUser = new User({username,password,role});
-            newUser.save(err=>{
-                if(err)
-                    res.status(500).json({ message : {msgBody : "Error has occured.", msgError : true }});
-                else
-                    res.status(201).json({ message : {msgBody : "Account successfully created!", msgError : false }});
-            });
-        }
-    })
-
-userRouter.post('/signin',passport.authenticate('local',{session : false}), (req, res)=>{
-    if(req.isAuthenticated()){
-        const {_id,username,role} = req.user;
+userRouter.get('/authenticated', passport.authenticate('local', { session: false }), (req, res) => {
+    if (req.isAuthenticated()) {
+        const { _id, email, role } = req.user;
         const token = signToken(_id);
-        res.cookie('access_token', token, {httpOnly: true, sameSite:true});
-        res.status(200).json({isAuthenticated : true, user : {username,role}});
+        res.cookie('access_token', token, { httpOnly: true, sameSite: true });
+        res.status(200).json({ isAuthenticated: true, user: { email, role } });
     }
 });
 
-userRouter.get('/signout',passport.authenticate('jwt',{session : false}), (req, res)=>{
-            res.clearCookie('access_token');
-            res.json({user:{username : "", role: ""},success : true});
-});
-
-// Creates new listing
-userRouter.get('/listing',passport.authenticate('jwt',{session : false}), (req, res)=>{
-    const listing = new Listing(req.body);
-    listing.save(err=>{
-        if(err)
-            res.status(500).json({message : {msgBody : "Error has occurred.", msgError : true }});
-        else{
-            req.user.listings.push(listing);
-            req.user.save(err=>{
-                if(err)
-                    res.status(500).json({messge : {mdgBody : "Error has occured.", msgError : true }});
+userRouter.post('/signup', (req, res) => {
+    const User = require('../models/user');
+    const { email, password, role } = req.body;
+    console.log(User)
+    User.findOne({ email }, (err, user) => {
+        if (err)
+            res.status(500).json({ message: { msgBody: "Error has occurred.", msgError: true } });
+        if (user)
+            res.status(400).json({ message: { msgBody: "Username is already taken.", msgError: true } });
+        else {
+            const newUser = new User({ email, password, role });
+            newUser.save(err => {
+                if (err)
+                    res.status(500).json({ message: { msgBody: "Error has occured.", msgError: true } });
                 else
-                    res.status(200).json({message : {msgBody : "Successfully created new listing!", msgError : false}});
+                    res.status(201).json({ message: { msgBody: "Account successfully created!", msgError: false } });
             });
         }
     })
-});
+
+    userRouter.post('/signin', passport.authenticate('local', { session: false }), (req, res) => {
+        if (req.isAuthenticated()) {
+            const { _id, email, role } = req.user;
+            const token = signToken(_id);
+            res.cookie('access_token', token, { httpOnly: true, sameSite: true });
+            res.status(200).json({ isAuthenticated: true, user: { email, role } });
+        }
+    });
+
+    userRouter.get('/signout', passport.authenticate('jwt', { session: false }), (req, res) => {
+        res.clearCookie('access_token');
+        res.json({ user: { username: "", role: "" }, success: true });
+    });
+
+    // Creates new listing
+    userRouter.get('/listing', passport.authenticate('jwt', { session: false }), (req, res) => {
+        const listing = new Listing(req.body);
+        listing.save(err => {
+            if (err)
+                res.status(500).json({ message: { msgBody: "Error has occurred.", msgError: true } });
+            else {
+                req.user.listings.push(listing);
+                req.user.save(err => {
+                    if (err)
+                        res.status(500).json({ messge: { mdgBody: "Error has occured.", msgError: true } });
+                    else
+                        res.status(200).json({ message: { msgBody: "Successfully created new listing!", msgError: false } });
+                });
+            }
+        })
+    });
 
 });
 
-userRouter.get('/listings',passport.authenticate('jwt',{session : false}), (req, res)=>{
-    User.findById({_id : req.user._id}).populate('listings').exec((err,document)=>{
-        if(err)
-            res.status(500).json({message : {msgBody : "Error has occurred.", msgError : true}});
-        else{
-            res.status(200).json({listings : document.listings, authenicated : true});
+userRouter.get('/listings', passport.authenticate('jwt', { session: false }), (req, res) => {
+    const User = require('../models/user');
+    User.findById({ _id: req.user._id }).populate('listings').exec((err, document) => {
+        if (err)
+            res.status(500).json({ message: { msgBody: "Error has occurred.", msgError: true } });
+        else {
+            res.status(200).json({ listings: document.listings, authenicated: true });
         }
     });
 });
 
-// admin panel
-userRouter.get('/admin',passport.authenticate('jwt',{session : false}), (req, res)=>{
-    if(req.user.role === 'admin'){
-        res.status(200).json({message : {msgBody : "You are an admin", msgError : false}});
-    }else{
-        res.status(403).json({message : {msgBody : "Scram you impasta!!", msgError: true}});
+// admin panel (for future use)
+userRouter.get('/admin', passport.authenticate('jwt', { session: false }), (req, res) => {
+    if (req.user.role === 'admin') {
+        res.status(200).json({ message: { msgBody: "You are an admin", msgError: false } });
+    } else {
+        res.status(403).json({ message: { msgBody: "Scram you impasta!!", msgError: true } });
     }
 });
 
-userRouter.get('/authenicated', passport.authenticate('jwt', {session : false}), (req, res)=>{
-    const {username,role} =req.user;
-    res.status(200).json({isAuthenticated : true, user : {username,role}});
+userRouter.get('/authenticated', passport.authenticate('jwt', { session: false }), (req, res) => {
+    const { username, role } = req.user;
+    res.status(200).json({ isAuthenticated: true, user: { username, role } });
 });
 
 module.exports = userRouter;

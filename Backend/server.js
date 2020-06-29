0618ad93b691ef19d129cd1f975c57093a27e013
vsaleem <vic.saleem@gmail.com>
2020-06-29 19:06:45 -0400

    Bug fix; moved userRouter above app.use(routes);

--- Backend/server.js
+++ Backend/server.js
@@ -13,14 +13,17 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+const userRouter = require('./routes/user');
+app.use('/user', userRouter);
+
 // Add routes, both API and view
 app.use(routes);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/listings", {
 });
 
-const userRouter = require('./routes/user');
-app.use('/user', userRouter);
+
 
 app.listen(PORT, function() {
     console.log(`🌎 ==> API server now on port ${PORT}!`);

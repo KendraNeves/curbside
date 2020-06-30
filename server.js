6b50733aa60e654d48fefd21a77b292455ca38f9
Ricardo Shaffer <ricardo.ramiro.shaffer@gmail.com>
2020-06-29 22:10:28 -0400

    added new routes to server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require("./routes");
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// require('./models/mlab');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -12,6 +12,9 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 // Add routes, both API and view
 app.use(routes);
 

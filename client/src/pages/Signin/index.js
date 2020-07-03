f826e2615cbb4b602f13c6e576da1907e7058b27
Marian Akiwumi <mfakiwumi1992@yahoo.com>
2020-07-02 22:46:54 -0400

    changed signin/up design

--- client/src/pages/Signin/index.js
+++ client/src/pages/Signin/index.js
@@ -1,9 +1,11 @@
 import React, {useState,useContext} from "react";
 import AuthServices from '../../Services/AuthServices';
 import AuthContext from '../../Context/AuthContext';
-import Message from '../../components/Message/index';
-import signin_img from "../../assets/images/img1.png";
+// import Message from '../../components/Message/index';
+// import signin_img from "../../assets/images/img1.png";
 import {Link} from "react-router-dom";
+import './signin.css';
+
 
 function Signin(props) {
   const [user,setUser] = useState({email: "", password: ""});
@@ -59,33 +61,83 @@ const onSubmit = e =>{
 
   render() { */}
     return (
-      <div className="log">
-        <header className="log-header">
-          <a className="log-brand" href="/">CURBSIDE  </a>
-        </header>
+      <div className="container-fluid">
+                <div className="row">
+
+                    <div className="col-sm-6 px-0 d-none d-sm-block">
+                        <img className="login-img" />
+                    </div>
+                    <div className="col-sm-6 login-section-wrapper">
+                        <div className="brand-wrapper">
+                          <h4><a className="log-brand" href="/">CURBSIDE  </a></h4>
+                        {/* <img src="assets/images/logo.svg" alt="logo" className="logo" /> */}
+                        </div>
+                        <div className="login-wrapper my-auto">
+                        <h1 className="login-title">Log in</h1>
+                        <form className="formbox" onSubmit={onsubmit}>
+                            <div className="form-group">
+                            <label htmlFor="email">Email</label>
+                            <input type="email" className="form-control" id="email" name="email" placeholder="Email..." />
+                            {/* <input className="form-input"
+                                type="text"
+                                id="email"
+                                name="email"
+                                placeholder="email"
+                                value={this.state.email}
+                                onChange={this.handleChange}
+                            /> */}
+                            </div>
+                            <div className="form-group mb-4">
+                            <label htmlFor="password">Password</label>
+                            <input type="password" class="form-control" id="password" name="password" placeholder="Password..."/>
+                            {/* <input className="form-input"
+                                placeholder="password"
+                                type="password"
+                                name="password"
+                                value={this.state.password}
+                                onChange={this.handleChange}
+                            /> */}
+                            </div>
+                            <input className="btn btn-dark" 
+                            type="submit" />
+                        </form>
+                        
+                        <p className="login-wrapper-footer-text">Don't have an account? <Link id="signin-link" to="/signup">Register here</Link></p>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+
+
+
+      // <div className="log">
+      //   <header className="log-header">
+      //     <a className="log-brand" href="/">CURBSIDE  </a>
+      //   </header>
       
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-6 img-holder">
-            </div>
-            <div className="col-6">
-              <h2 className="log-title">SIGN IN</h2>
-                <form onSubmit={onSubmit}>
-                  <div className="form-group">
-                    <input type="email" class="form-control" id="email" name="email" placeholder="Email..." />
-                  </div>
-                  <div className="form-group">
-                    <input type="password" class="form-control" id="password" name="password" placeholder="Password..."/>
-                  </div>
-                  <button type="submit" className="btn">LOG IN</button>
-                  <br></br>
-                  <Link id="signin-link" to="/signup">SIGN UP</Link>
-              </form>
-              {message ? <Message message={message} /> : null}
-            </div>
-          </div>
-        </div>
-      </div>
+      //   <div className="container-fluid">
+      //     <div className="row">
+      //       <div className="col-6 img-holder">
+      //       </div>
+      //       <div className="col-6">
+      //         <h2 className="log-title">SIGN IN</h2>
+      //           <form onSubmit={onSubmit}>
+      //             <div className="form-group">
+      //               <input type="email" className="form-control" id="email" name="email" placeholder="Email..." />
+      //             </div>
+      //             <div className="form-group">
+      //               <input type="password" class="form-control" id="password" name="password" placeholder="Password..."/>
+      //             </div>
+      //             <button type="submit" className="btn">LOG IN</button>
+      //             <br></br>
+      //             <Link id="signin-link" to="/signup">SIGN UP</Link>
+      //         </form>
+      //         {message ? <Message message={message} /> : null}
+      //       </div>
+      //     </div>
+      //   </div>
+      // </div>
     )
 }
 export default Signin;
--- client/src/pages/Signup/index.js
+++ client/src/pages/Signup/index.js
@@ -44,49 +44,100 @@ function Signup(props) {
   
 
   return(
-    <div className="log">
-      <header className="log-header">
-        <a className="log-brand" href="/">CURBSIDE</a>
-      </header>
+    <div className="container-fluid">
+    <div className="row">
 
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-6">
-            <img src={signup_img} alt="boxes toppled over"></img>
-          </div>
-          <div className="col-6">
-            <div className="row">
-              <h2 className="log-title">CREATE AN ACCOUNT</h2>
-            </div>
-            <div className="row">
-              <form onSubmit={onSubmit}>
-                  <div className="form-group">
-                    <input type="email" name="email" 
-                            value={user.email}
-                            onChange={onChange}
-                            className="form-control" 
-                            id="exampleInputEmail1" 
-                            aria-describedby="emailHelp" 
-                            placeholder="Email"></input>
-                  </div>
-                  <div className="form-group">
-                    <input type="password" name="password" 
-                            value={user.password}
-                            onChange={onChange}
-                            className="form-control" 
-                            id="exampleInputPassword1" 
-                            placeholder="Password"></input>
-                  </div>
-                  <button type="submit" className="btn">SIGN UP</button>
-                  <br></br>
-                  <Link id="signin-link" to="/signin">SIGN IN</Link>
-              </form>
-              {message ? <Message message={message}/> : null}
-            </div>
-          </div>
+        <div className="col-sm-6 px-0 d-none d-sm-block">
+            <img className="login-img" />
         </div>
-      </div>
+        <div className="col-sm-6 login-section-wrapper">
+            <div className="brand-wrapper">
+              <h4><a className="log-brand" href="/">CURBSIDE  </a></h4>
+            {/* <img src="assets/images/logo.svg" alt="logo" className="logo" /> */}
+            </div>
+            <div className="login-wrapper my-auto">
+            <h1 className="login-title">Create an account</h1>
+            <form className="formbox" onSubmit={onsubmit}>
+                <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <input type="email" className="form-control" id="email" name="email" placeholder="Email..." />
+                {/* <input className="form-input"
+                    type="text"
+                    id="email"
+                    name="email"
+                    placeholder="email"
+                    value={this.state.email}
+                    onChange={this.handleChange}
+                /> */}
+                </div>
+                <div className="form-group mb-4">
+                <label htmlFor="password">Password</label>
+                <input type="password" class="form-control" id="password" name="password" placeholder="Password..."/>
+                {/* <input className="form-input"
+                    placeholder="password"
+                    type="password"
+                    name="password"
+                    value={this.state.password}
+                    onChange={this.handleChange}
+                /> */}
+                </div>
+                <input className="btn btn-dark" 
+                type="submit" />
+            </form>
+            
+            <p className="login-wrapper-footer-text">Already have an account? <Link id="signin-link" to="/signin">Sign in</Link></p>
+            </div>
+        </div>
+        
     </div>
+</div>
+
+    
+    
+    
+    // <div className="log">
+    //   <header className="log-header">
+    //     <a className="log-brand" href="/">CURBSIDE</a>
+    //   </header>
+
+    //   <div className="container-fluid">
+    //     <div className="row">
+    //       <div className="col-6">
+    //         <img src={signup_img} alt="boxes toppled over"></img>
+    //       </div>
+    //       <div className="col-6">
+    //         <div className="row">
+    //           <h2 className="log-title">CREATE AN ACCOUNT</h2>
+    //         </div>
+    //         <div className="row">
+    //           <form onSubmit={onSubmit}>
+    //               <div className="form-group">
+    //                 <input type="email" name="email" 
+    //                         value={user.email}
+    //                         onChange={onChange}
+    //                         className="form-control" 
+    //                         id="exampleInputEmail1" 
+    //                         aria-describedby="emailHelp" 
+    //                         placeholder="Email"></input>
+    //               </div>
+    //               <div className="form-group">
+    //                 <input type="password" name="password" 
+    //                         value={user.password}
+    //                         onChange={onChange}
+    //                         className="form-control" 
+    //                         id="exampleInputPassword1" 
+    //                         placeholder="Password"></input>
+    //               </div>
+    //               <button type="submit" className="btn">SIGN UP</button>
+    //               <br></br>
+    //               <Link id="signin-link" to="/signin">SIGN IN</Link>
+    //           </form>
+    //           {message ? <Message message={message}/> : null}
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
   )
 }
 

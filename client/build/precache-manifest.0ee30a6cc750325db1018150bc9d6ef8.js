d3c4702835b0fa6ecd5597ad51d9277ad746aeb4
Robert Wongus <rob_wo@yahoo.com>
2020-07-06 19:21:26 -0400

    change gitignore build

---
+++ client/build/precache-manifest.0ee30a6cc750325db1018150bc9d6ef8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dbd58bb5eaf61109fd35839df75b725",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8187ecf8ecddb11bbe58",
+    "url": "/static/css/2.09d1976d.chunk.css"
+  },
+  {
+    "revision": "7c9ae9b88fd8ce0787c2",
+    "url": "/static/css/main.ed72fca3.chunk.css"
+  },
+  {
+    "revision": "8187ecf8ecddb11bbe58",
+    "url": "/static/js/2.2ab7b81a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.2ab7b81a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c9ae9b88fd8ce0787c2",
+    "url": "/static/js/main.37131537.chunk.js"
+  },
+  {
+    "revision": "9ec376ad0874c23c63f4",
+    "url": "/static/js/runtime-main.fd5198c8.js"
+  },
+  {
+    "revision": "10696c917f35566971f2a8b9dd5d97c0",
+    "url": "/static/media/homepage.10696c91.jpg"
+  },
+  {
+    "revision": "10ea94babb9052a1c1071d1ad80c95b8",
+    "url": "/static/media/img1.10ea94ba.png"
+  }
+]);

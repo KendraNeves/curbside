588f443d333cdb628f9bb42e3a8a2ebdf784d690
Robert Wongus <rob_wo@yahoo.com>
2020-07-06 18:41:49 -0400

    mongo

--- config/index.js
+++ config/index.js
@@ -2,6 +2,6 @@ const dbuser = Curbside
 
 const dbpassword = OnTheCorner1
 
-const MONGOD_URI = 'mongodb://<dbuser>:<dbpassword>@ds155418.mlab.com:55418/heroku_572w7dcj'
+const MONGODB_URI = 'mongodb://<dbuser>:<dbpassword>@ds155418.mlab.com:55418/heroku_572w7dcj'
 
 module.exports = MONGODB_URI

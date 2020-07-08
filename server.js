8308f41603e977d0690fa7b703dec0ddefc17bcc
vsaleem <vic.saleem@gmail.com>
2020-07-08 14:05:25 -0400

    Added app.get response for all pages deployed on Heroku.

--- server.js
+++ server.js
@@ -18,6 +18,10 @@ app.use(bodyParser.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+  });
 }
 
 // User route

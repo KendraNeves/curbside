36733eb51c430e6a5ca4dd2d7b97e00cf7fdaf16
vsaleem <vic.saleem@gmail.com>
2020-07-08 14:39:16 -0400

    Testing deployment to Heroku.

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ app.use(bodyParser.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  
+  app.use(express.static("build"));
+
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "build", "index.html"));
   });
